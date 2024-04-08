af2c389a4a8788a0f0631e597167600433be076b
michaelorina <orinamichael4@gmail.com>
2024-04-08 10:24:49 +0300

    final commit!

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,10 @@
+import styles from './footer.module.css'
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>Michael O</div>
+            <div className={styles.text}>All Rights Reserved</div>
+        </div>
+    )
+}
+export default Footer
